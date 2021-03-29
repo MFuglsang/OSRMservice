04adce899c067084850886cc40394956488f7063
MFuglsang <morten.w.fuglsang@gmail.com>
2021-03-29 18:44:13 +0200

    Installe/uninstall definitions

---
+++ bin/installservice_p.cmd
@@ -0,0 +1,2 @@
+C:\app\OSRMservice\app\winsw\osrmservice.exe install C:\app\OSRMservice\app\winsw\osrmservice.xml
+pause
---
+++ bin/uninstallservice_p.cmd
@@ -0,0 +1,2 @@
+C:\app\OSRMservice\app\winsw\osrmservice.exe uninstall C:\app\OSRMservice\app\winsw\osrmservice.xml
+pause
